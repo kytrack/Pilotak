c1243181cc891003107bbba2863b833bf29d85a8
kytrack <vkrisztianmgy@gmail.com>
2023-05-19 09:59:21 +0200

    szunet elotti allas

---
+++ Pilotak/Pilotaclass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilotak
+{
+    class Pilotaclass
+    {
+        string nev;
+        string szuletesidatum;
+        string nemzetiseg;
+        int rajtszam;
+
+        public Pilotaclass(string nev, string szuletesidatum, string nemzetiseg, int rajtszam)
+        {
+            this.nev = nev;
+            this.szuletesidatum = szuletesidatum;
+            this.nemzetiseg = nemzetiseg;
+            this.rajtszam = rajtszam;
+        }
+
+        public string Nev { get => nev;}
+        public string Szuletesidatum { get => szuletesidatum;}
+        public string Nemzetiseg { get => nemzetiseg;}
+        public int Rajtszam { get => rajtszam;}
+    }
+}
--- Pilotak/Program.cs
+++ Pilotak/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using Pilotak;
+
+List<Pilotaclass> pilotak = new List<Pilotaclass>();
+
+StreamReader sr = new StreamReader("pilotak.csv", encoding:"UTF8");
+sr.ReadLine();
+
+while (!sr.EndOfStream)
+{
+    string[] adatok = sr.ReadLine().Split(";");
+    pilotak.Add(new Pilotaclass(adatok[0], adatok[1], adatok[2], int.Parse(adatok[3])));
+}
+sr.Close();
+
+Console.WriteLine($"3.feladat: {pilotak.Count()}");
